c7eb407ed170f73f3c520d4dfbbd3bf9d7e6c940
Julien GOLETTO <julien.goletto@gmail.com>
2022-02-21 16:54:42 +0100

    formatage des objets movie terminé pour la méthode findAll

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -3,7 +3,7 @@ const {Country, Genre, Movie, Review, Season, User} = require('../models/tableIn
 const mainController = {
     async mainPage (req, res) {
 
-        const movies = await Movie.findAll({
+        const data = await Movie.findAll({
             attributes : ['french_title','directors','release_year','length'],
             include : [
                 {
@@ -19,12 +19,34 @@ const mainController = {
                 {
                     model : Genre,
                     as : 'movieGenres',
-                    attribute : ['name']
+                    attributes : ['name']
                 }
         ]
         });
-        console.log(movies);
+
+        let movies = [];
+
+        for (let movie of data){
+            const movieData = movie.dataValues;
+            const keys = Object.keys(movieData);
+            let movieToPush = {};
+            for (let key of keys){
+                if(key === 'recommendedByUser'){
+                    movieToPush[key] = movieData[key].dataValues.pseudo;
+                }else if (key === 'producedByCountries' || key === 'movieGenres'){
+                    movieToPush[key] = [];
+                    for (info of movieData[key]){
+                        movieToPush[key].push(info.dataValues.name);
+                    }
+                }
+                else{
+                    movieToPush[key] = movieData[key];
+                }
+            }
+            movies.push(movieToPush);
+        }
         res.send(movies);
+        console.log(movies);
     },
 };
 
